8ae6b54721ad3a1f140be26f8584c2d0643ae7fe
R Pooran Prasad <pooran@live.com>
2015-03-29 17:43:34 +0530

    SQLite 3.0.0 test

---
+++ SQLite3Test/SQLite3Test.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SQLite3Test.WinPhone.Resources;
+
+namespace SQLite3Test.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
